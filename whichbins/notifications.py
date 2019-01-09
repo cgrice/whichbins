2db13afa71690d51aebe64ea721f9da4a80c03a5
Chris Grice <chris@chrisgrice.com>
2019-01-09 19:13:03 +0000

    Making our secrets secret

--- whichbins/notifications.py
+++ whichbins/notifications.py
@@ -1,15 +1,21 @@
+import os
+
 from twilio.rest import Client
 
 def send_sms(message, recipient):
     # Your Account Sid and Auth Token from twilio.com/console
-    account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-    auth_token = 'your_auth_token'
+    account_sid = os.environ.get('TWILIO_ACCOUNT_SID', 'ACXXXXX')
+    auth_token = os.environ.get('TWILIO_AUTH_TOKEN', 'XXXXXX')
     # Your messaging service Sid
-    service_sid = 'MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    service_sid = os.environ.get('TWILIO_MSGSERVICE_SID', 'MSGXXXXX')
+    # Your twilio phone number
+    sender = os.environ.get('TWILIO_SENDER', '+44000000')
+    
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
         body=message,
         messaging_service_sid=service_sid,
+        from_=sender,
         to=recipient
     )
